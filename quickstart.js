b15159226697bbeeb987fac9de740ed05a29d70c
Vd3185 <32833000+Vd3185@users.noreply.github.com>
2018-10-19 22:04:35 +0530

    Add files via upload

--- quickstart.js
+++ quickstart.js
@@ -1,9 +1,17 @@
-const {Resource} = require('@google-cloud/resource')
+// [START resource_quickstart]
+// Imports the Google Cloud client library
+const Resource = require('@google-cloud/resource')
+
+// Your Google Cloud Platform project ID
 const projectId = 'YOUR_PROJECT_ID'
+
+//Creates the Client			
 const resourceClient = new Resource({
   projectId: projectId,
 })
 
+
+// Lists current projects. This code was changed from promise to async and await on 10/19/2018 - Vishal Deshmukh
 const run = async () => {
   try {
     const results = await resourceClient.getProjects()
@@ -15,3 +23,6 @@ const run = async () => {
     console.error('ERROR:', err)
   }
 }
+
+run();
+// [END resource_quickstart]
